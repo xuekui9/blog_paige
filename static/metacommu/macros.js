4ba3ad3ada479912571d8c7221afda3ccda2e8cb
xuekui9 <wsgwhym@163.com>
2023-03-26 16:22:22 +0800

    chat sign up

--- static/metacommu/macros.js
+++
@@ -1,9 +0,0 @@
-remark.macros.scale = function (val) {
- var url = this;
- return '<img src="' + url + '" style="width: ' + val + '" />';
-};
-
-remark.macros['wlt'] = function(width, left, top, cl="") {
-  var url = this;
-  return '<img src="' + url + '" style="position:absolute;left:' + left + ';top:' + top + ';width:' + width + '" class="' + cl + '" />';
-};
